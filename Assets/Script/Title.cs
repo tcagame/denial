1d06fecca25ef3baf3c75ca700e485843d6a59ae
ito <i>
2017-07-21 17:45:51 +0900

    sceneリファクタリング

---
+++ Assets/Script/Title.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Title : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    public void moveGemeScene( ) {
+        SceneManager.LoadScene("GameScene");
+    }
+}
