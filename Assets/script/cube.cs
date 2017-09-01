bd3996c5aca31cc3572abe91773d6a8e7f3c9246
伊藤翔太 <i>
2017-09-02 04:33:17 +0900

    オブジェクト生成

--- Assets/script/cube.cs
+++ Assets/script/cube.cs
@@ -10,20 +10,23 @@ public class Cube : MonoBehaviour {
         m_mouseDownPosition = transform.position;
     }
 
-    void OnMouseDrag()
+    void OnMouseOver()
     {
         // マウスクリックした場所をワールド座標に変化して、
         // 初期位置とマウスクリック位置の中間にオブジェクトを配置。
         // オブジェクトのスケールを初期位置とマウスクリックの距離に。
         // オブジェクトの向きをマウスクリックした位置に。
-    
-        Vector3 inputPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 9.5f);
-        Vector3 mousePos = Camera.main.ScreenToWorldPoint(inputPosition);
-        Vector3 mediumPos = (mousePos - m_mouseDownPosition) / 2.0f + m_mouseDownPosition;
-        float dist = Vector3.Distance(mousePos, m_mouseDownPosition);
-    
-        transform.position = mediumPos;
-        transform.localScale = new Vector3(1.0f, 1.0f, dist);
-        transform.LookAt(mousePos);
+
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 inputPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 9.5f);
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(inputPosition);
+            Vector3 mediumPos = (mousePos - m_mouseDownPosition) / 2.0f + m_mouseDownPosition;
+            float dist = Vector3.Distance(mousePos, m_mouseDownPosition);
+
+            transform.position = mediumPos;
+            transform.localScale = new Vector3(1.0f, 1.0f, dist);
+            transform.LookAt(mousePos);
+        }
     }
 }
