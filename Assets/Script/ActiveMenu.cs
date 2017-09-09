90179483c26708aea4de5624230e8947f3707893
suzuki <a>
2017-09-09 09:12:04 +0900

    menu追加

---
+++ Assets/Script/ActiveMenu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ActiveMenu : MonoBehaviour {
+	[SerializeField] GameObject start;
+	[SerializeField] GameObject menu;
+
+	public void PushStartbutton() {
+		start.SetActive (false);
+		menu.SetActive (true);
+	}
+}
---
+++ Assets/Script/Fade.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Fade : MonoBehaviour {
+	[SerializeField] Image image;
+	private float alpha;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
