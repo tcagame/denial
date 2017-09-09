34cbe8bebf459cbc5525c68886e02fe097bbcbc4
suzuki <a>
2017-09-09 14:02:32 +0900

    test

---
+++ Assets/Script/BackTitle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackTitle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ( Input.GetMouseButtonUp ( 0 ) ) {
+			SceneManager.LoadScene ("Title");
+		}
+		
+	}
+		
+}
