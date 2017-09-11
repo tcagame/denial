709c0354b453595410290436fb52904eff8622ea
suzuki <a>
2017-09-11 20:55:54 +0900

    fade修正, チュートリアル差し替え

--- Assets/Script/Brave.cs
+++ Assets/Script/Brave.cs
@@ -21,18 +21,8 @@ public class Brave : MonoBehaviour {
 
 	void OnCollisionStay( Collision collision ) {
 		if (collision.gameObject.name == "boss") {
-<<<<<<< HEAD
-
-            SceneManager.LoadScene (nextStage);
-=======
-			fade.gameObject.SetActive (true);
-<<<<<<< HEAD
 			StartCoroutine (FadeDelay(1.5f, _fade.fadeStart));
 			StartCoroutine (SceneLoad (2.5f, nextStage));
-=======
-			StartCoroutine (SceneLoad (1.5f, nextStage));
->>>>>>> 48e3077e8e6b550e4a22df6f49f6b762a4f55d70
->>>>>>> d419bb4c745a46f5442120f3deff035d70fd6c80
 		}
 		if (collision.gameObject.name == "boss2") {
 
@@ -49,6 +39,7 @@ public class Brave : MonoBehaviour {
 	private IEnumerator FadeDelay( float time, string fadetype ) {
 		yield return new WaitForSeconds (time);
 
+		fade.gameObject.SetActive (true);
 		_fade.FadeSwitch (fadetype);
 	}
 		
