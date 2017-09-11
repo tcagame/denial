50256ddc0abf8d163f89dc1c7bc1d9501d77ecc3
suzuki <a>
2017-09-11 20:48:36 +0900

    FadeDelay追加

--- Assets/Script/Brave.cs
+++ Assets/Script/Brave.cs
@@ -7,9 +7,11 @@ public class Brave : MonoBehaviour {
 	[SerializeField] string nextStage;
 	[SerializeField] GameObject fade;
 
+	Fade _fade;
+
 	// Use this for initialization
-	void Start () {
-		
+	void Start ( ) {
+		_fade = fade.gameObject.GetComponent<Fade> ();
 	}
 	
 	// Update is called once per frame
@@ -20,7 +22,8 @@ public class Brave : MonoBehaviour {
 	void OnCollisionStay( Collision collision ) {
 		if (collision.gameObject.name == "boss") {
 			fade.gameObject.SetActive (true);
-			StartCoroutine (SceneLoad (1.5f, nextStage));
+			StartCoroutine (FadeDelay(1.5f, _fade.fadeStart));
+			StartCoroutine (SceneLoad (2.5f, nextStage));
 		}
 		if (collision.gameObject.name == "boss2") {
 			SceneManager.LoadScene ("StuffedRoll");
@@ -31,6 +34,12 @@ public class Brave : MonoBehaviour {
 		yield return new WaitForSeconds (time);
 
 		SceneManager.LoadScene (name);
+	}
+
+	private IEnumerator FadeDelay( float time, string fadetype ) {
+		yield return new WaitForSeconds (time);
+
+		_fade.FadeSwitch (fadetype);
 	}
 		
 }
--- Assets/Script/Fade.cs
+++ Assets/Script/Fade.cs
@@ -8,7 +8,7 @@ public class Fade : MonoBehaviour {
 	private Image image;
 	private Color alpha;
 	private float startTime;
-	[SerializeField] private string fadeStart;
+	public string fadeStart;
 	//[SerializeField] string name;
 	[SerializeField] private float fadeTime;
 
@@ -21,6 +21,15 @@ public class Fade : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		
+		/*if (image.color.a == 0) {
+			SceneManager.LoadScene (name);
+			startTime = Time.time;
+			fadeStart = "FadeIn";
+		}*/
+	}
+
+	public void FadeSwitch( string fadetype ) {
 		switch (fadeStart) {
 		case "fadein":
 			alpha.a = 1.0f - (Time.time - startTime) / fadeTime;
@@ -33,11 +42,6 @@ public class Fade : MonoBehaviour {
 			Debug.Log (image.color.a);
 			break;
 		}
-		/*if (image.color.a == 0) {
-			SceneManager.LoadScene (name);
-			startTime = Time.time;
-			fadeStart = "FadeIn";
-		}*/
 	}
 		
 }
