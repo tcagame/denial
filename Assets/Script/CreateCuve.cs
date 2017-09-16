201398c767d3ae40395b043199640664550c6c4f
MasakazuKinpara <ebipilaf000@gmail.com>
2017-09-16 15:05:35 +0900

    taima-

--- Assets/Script/CreateCuve.cs
+++ Assets/Script/CreateCuve.cs
@@ -7,26 +7,34 @@ public class CreateCuve : MonoBehaviour
     // クリックした位置座標
     private Vector3 clickPosition;
 	[SerializeField]private GameObject cube;
+	private const float count = 5;
+	private float countDown;
+	[SerializeField] GameObject player;
 
     // Use this for initialization
     void Start()
     {
-
+		countDown = count;
     }
 
     // Update is called once per frame
     void Update() { 
-		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-		RaycastHit hit = new RaycastHit();
-		if (!Physics.Raycast (ray, out hit)) {
-			if (Input.GetMouseButtonDown (0)) {
-				clickPosition = Input.mousePosition;
-				// Z軸修正
-				clickPosition.z = 10f;
-				// オブジェクト生成 : オブジェクト(GameObject), 位置(Vector3), 角度(Quaternion)
-				// ScreenToWorldPoint(位置(Vector3))：スクリーン座標をワールド座標に変換する
-				cube = (GameObject)Resources.Load ("Prefab/BelconTest");
-				Instantiate (cube, Camera.main.ScreenToWorldPoint (clickPosition), cube.transform.rotation);
+		countDown -= Time.deltaTime;
+		countDown = Mathf.Max (countDown, 0, 0);
+
+		if (((int)countDown) <= 0) {
+			RaycastHit hit = new RaycastHit();
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			if (!Physics.Raycast (ray, out hit)) {
+				if (Input.GetMouseButtonDown (0)) {
+					clickPosition = Input.mousePosition;
+					// Z軸修正
+					clickPosition.z = 10f;
+					// オブジェクト生成 : オブジェクト(GameObject), 位置(Vector3), 角度(Quaternion)
+					// ScreenToWorldPoint(位置(Vector3))：スクリーン座標をワールド座標に変換する
+					cube = (GameObject)Resources.Load ("Prefab/BelconTest");
+					Instantiate (cube, Camera.main.ScreenToWorldPoint (clickPosition), cube.transform.rotation);
+				}
 			}
 		}
     }
