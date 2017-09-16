e69edab355bb1bb13f7804f61c6e39394fc8d2e9
suzuki <a>
2017-09-16 18:26:35 +0900

    ベルコンバグ修正

--- Assets/Script/CreateCuve.cs
+++ Assets/Script/CreateCuve.cs
@@ -19,22 +19,23 @@ public class CreateCuve : MonoBehaviour
 
     // Update is called once per frame
     void Update() { 
-		//countDown -= Time.deltaTime;
-		//countDown = Mathf.Max (countDown, 0, 0);
-
-		//if (((int)countDown) <= 0) {
+		countDown -= Time.deltaTime;
+		countDown = Mathf.Max (countDown, 0, 0);
+		//Physics.SphereCast(Ray ray, ①float radius, out RaycastHit hit, float maxDistance, int layerMask)
+		if (((int)countDown) <= 0) {
+			clickPosition = Input.mousePosition;
 			RaycastHit hit = new RaycastHit();
-			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-			if (!Physics.Raycast (ray, out hit)) {
+			Ray ray = Camera.main.ScreenPointToRay(clickPosition);
+			if (!Physics.SphereCast (ray, 0.7f, out hit)) {
 				if (Input.GetMouseButtonDown (0)) {
-					clickPosition = Input.mousePosition;
+					
 					// Z軸修正
 					clickPosition.z = 10f;
 					// オブジェクト生成 : オブジェクト(GameObject), 位置(Vector3), 角度(Quaternion)
 					// ScreenToWorldPoint(位置(Vector3))：スクリーン座標をワールド座標に変換する
 					cube = (GameObject)Resources.Load ("Prefab/BelconTest");
 					Instantiate (cube, Camera.main.ScreenToWorldPoint (clickPosition), cube.transform.rotation);
-				//}
+				}
 			}
 		}
     }
