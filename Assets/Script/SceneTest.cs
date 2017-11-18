de114f3932dbe2d268710d6ebba64203cc92f4c6
suzuki <a>
2017-11-18 15:53:38 +0900

    仮

---
+++ Assets/Script/SceneTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneTest : MonoBehaviour {
+
+	string scenename;
+	Brave _brave;
+
+	// Use this for initialization
+	void Start () {
+		DontDestroyOnLoad (this.gameObject);
+		_brave = GameObject.Find("Brave").GetComponent<Brave> ();
+		//GameObject _brave = GetComponent<Brave> ();
+		scenename = _brave.getReTryScene();
+		Debug.Log (scenename);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
