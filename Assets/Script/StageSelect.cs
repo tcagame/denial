4e89374b2265313b884ecb82d2a3c58a1fc7e4b8
student <student@MacBookPro-59.local>
2018-01-20 15:59:26 +0900

    修正

---
+++ Assets/Script/StageSelect.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StageSelect : MonoBehaviour {
+	[SerializeField] string name;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void SelectStage() {
+		SceneManager.LoadScene(name);
+	}
+}
